8fee3e8398604fb951b54f1c2c35fd304d114a12
Adam Cullen <hungrydeveloper@gmail.com>
2018-07-08 22:30:56 +0100

    Initial sln and proj set up

---
+++ src/Ewok.Core/MyTestValues.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ewok.Core
+{
+    public class MyTestValues
+    {
+        public string[] SomeStrings
+        {
+            get
+            {
+                return new[] { "Value1", "Value2" };
+            }
+        }
+    }
+}
